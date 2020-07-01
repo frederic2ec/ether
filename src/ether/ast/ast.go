009744cc64199a7152d313b929bf86a12a3ad5ee
frederic2ec <freddy.cho@hotmail.com>
2020-07-01 16:43:40 -0400

    AST

---
+++ src/ether/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "ether/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type VarStatement struct {
+	Token token.Token // the token.VAR token
+	Name  *Identifier
+	value Expression
+}
+
+func (ls *VarStatement) StatementNode()       {}
+func (ls *VarStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
