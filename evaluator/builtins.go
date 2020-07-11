8647d6d8651f8467b451f9ab5230cdde1d3962b1
frederic2ec <freddy.cho@hotmail.com>
2020-07-11 16:19:16 -0400

    puts and print builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"ether/object"
+	"fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -79,6 +80,24 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Print(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
