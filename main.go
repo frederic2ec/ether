6c8b15292bf52a4433293cb8695e07837183ce04
frederic2ec <freddy.cho@hotmail.com>
2020-07-27 17:13:13 -0400

    :sparkles: Support for executing file / Improved REPL

--- main.go
+++ main.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	"ether/repl"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/user"
+	"path"
 )
 
+var (
+	interactive bool
+	version     bool
+)
+
+// Init flag
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] [<filename>]\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	flag.BoolVar(&version, "v", false, "display version information")
+
+	flag.BoolVar(&interactive, "i", false, "enable interactive mode")
+}
+
+// Launch main program
 func main() {
+	flag.Parse()
+
+	if version {
+		fmt.Printf("Ether %s \n", FullVersion())
+		os.Exit(0)
+	}
+
 	user, err := user.Current()
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not determine current user: %s", err)
 	}
-	fmt.Printf("Hello %s! This is the Ether programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	args := flag.Args()
+	opts := &repl.Options{
+		Interactive: interactive,
+	}
+	repl := repl.New(user.Username, args, opts)
+	repl.Run()
 }
